3186b5e146be5b7600a901501c03065ef832e6a8
Cuong Nguyen <cuongpianna1996@gmail.com>
2018-11-06 17:52:33 +0700

    Fixed get posts func()

--- app/post/views.py
+++ app/post/views.py
@@ -2,13 +2,15 @@ from flask import request
 from app.post.serializers import PostSchema
 from app.post.models import Post
 from app.post import bp
+from app.helpers.extensions import db
 
 post_schema = PostSchema()
+post_schema = PostSchema(many=True)
 
 
 @bp.route('/api/posts', methods=['GET'])
 def get_posts():
-    posts = Post.query.get(1)
+    posts = Post.query.all()
     print(posts)
     if posts:
         return post_schema.dumps(posts)
@@ -18,6 +20,9 @@ def get_posts():
 
 @bp.route('/api/posts', methods=['POST'])
 def insert_post():
-    content = request.json()
-    print(content)
+    content = request.get_json()
+    post = Post(title=content['title'], body=content['body'], sub_body='test')
+    db.session.add(post)
+    db.session.commit()
+    return 'ok'
 
