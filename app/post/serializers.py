cb98f1a904c2afee02c1fc672d1d4defc0578ec6
cuongpianna <cuongpianna1996@gmail.com>
2018-11-06 23:14:23 -0700

    Modified get, post method for post model

--- app/post/serializers.py
+++ app/post/serializers.py
@@ -1,7 +1,13 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates, ValidationError
 
 
 class PostSchema(Schema):
     title = fields.Str()
     body = fields.Str()
-    sub_body = fields.Str()
+
+    @validates('title')
+    def validate_title(self, value):
+        if len(value) < 6:
+            raise ValidationError('Title must be greater than 6')
+        elif len(value) > 64:
+            raise ValidationError('Title must not be greater than 64')
--- app/post/views.py
+++ app/post/views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from app.post.serializers import PostSchema
 from app.post.models import Post
 from app.post import bp
@@ -11,18 +11,51 @@ post_schema = PostSchema(many=True)
 @bp.route('/api/posts', methods=['GET'])
 def get_posts():
     posts = Post.query.all()
-    print(posts)
     if posts:
-        return post_schema.dumps(posts)
+        results = post_schema.dumps(posts)
+        return jsonify({
+            'status': 'ok',
+            'code': 200,
+            'data': results
+        }), 200
     else:
-        return 'test'
+        return jsonify({
+            'status': 'ok',
+            'code': 200,
+            'msg': 'No data found',
+            'data': []
+        }), 200
 
 
 @bp.route('/api/posts', methods=['POST'])
 def insert_post():
     content = request.get_json()
-    post = Post(title=content['title'], body=content['body'], sub_body='test')
-    db.session.add(post)
-    db.session.commit()
-    return 'ok'
-
+    try:
+        post = Post(title=content['title'], body=content['body'], sub_body='test')
+        data, errors = PostSchema().load(post.__dict__)
+        if errors:
+            return jsonify({
+                'status': 'ko',
+                'code': 422,
+                'msg': errors
+            }), 422
+    except:
+        return jsonify({
+            'status': 'ko',
+            'code': 415,
+            'msg': 'Fail'
+        }), 415
+    try:
+        db.session.add(post)
+        db.session.commit()
+        return jsonify({
+            'status': 'ok',
+            'code': 201,
+            'msg': 'Create a new post successfully!'
+        }), 201
+    except:
+        return jsonify({
+            'status': 'ko',
+            'code': 403,
+            'msg': 'Fail'
+        })
