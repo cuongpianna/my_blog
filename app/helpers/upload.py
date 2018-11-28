36948a20105f4199bc99d8ab9af962efe087fb10
Cuong Nguyen <cuongpianna1996@gmail.com>
2018-11-28 20:06:32 +0700

    Update upload.py

--- app/helpers/upload.py
+++ app/helpers/upload.py
@@ -3,6 +3,6 @@ import boto3
 
 s3 = boto3.resource(
     "s3",
-    aws_access_key_id='AKIAITVNJVWUSOEHP3JA',
-    aws_secret_access_key='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'
+    aws_access_key_id='',
+    aws_secret_access_key=''
 )
